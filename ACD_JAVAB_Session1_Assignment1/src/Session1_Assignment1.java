1190fe3448a4990adf36bbb3d2a1927a12d8770f
KamakshiAbbi <kamakshigit12@gmail.com>
2016-04-24 01:51:32 +0530

    ACD_JAVAB_Session1_Assignment1
    Sum of two numbers

---
+++ ACD_JAVAB_Session1_Assignment1/src/Session1_Assignment1.java
@@ -0,0 +1,12 @@
+
+//Session1_Assignment : 23.4.2016
+//Author: Kamakshi Abbi
+public class Session1_Assignment1 {
+	public static void main(String[] args) {
+		int num1 = 20;
+		float num2 = 4.2f;
+		int sum;
+		sum = num1 + (int)num2;
+		System.out.println("The sum of the two numbers "+ num1 + " and " + num2 +" is: "+sum);
+	}
+} 
